a8f37fc341164f08483e44c48033da551c386d08
LinaAlvarado <linamariaalvaradopallares@gmail.com>
2024-02-24 10:30:16 -0500

    Delete comments

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
 const urlInicio = `https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast`;
-
 const containerMeals = document.querySelector(".container-meals");
-
-// const fetchData = (urlApi) => {
-//   fetch(urlApi)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("La solicitud falló");
-//       }
-//       return response.json(); // Convertir la respuesta a JSON
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       let view = `${data.meals.map(
-//         (meal) => `
-//       <div>
-//           <div class="container-img">
-//             <img src="${meal.strMealThumb}" alt="Imagen de plato de comida">
-//           </div>
-//           <h3 class="title-meals">${meal.strMeal}</h3>
-//       </div>
-//       `
-//       )}`;
-
-//       containerMeals.innerHTML = view;
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// };
 
 const fetchData = async (urlApi) => {
   try {
@@ -60,31 +31,6 @@ const renderMeals =(dataMeals)=>{
 containerMeals.innerHTML = view;
 }
 
-
-// const fetchDataCategory = (categorieSelected) => {
-//   const URL_CATEGORY = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorieSelected}`;
-//   fetch(URL_CATEGORY)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//       let view = `${data.meals
-//         .map(
-//           (meal) => `
-//           <div>
-//               <div class="container-img">
-//                 <img src="${meal.strMealThumb}" alt="Imagen de plato de comida">
-//               </div>
-//               <h3 class="title-meals" >${meal.strMeal}</h3>
-//           </div>
-//           `
-//         )
-//         .slice(0, 12)}`;
-//       containerMeals.innerHTML = view;
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// };
 
 const fetchDataCategory = async (categorieSelected) => {
   const URL_CATEGORY = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorieSelected}`;
